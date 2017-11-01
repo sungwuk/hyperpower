3dd28808cff007958d077ad0ec96025cc84411c9
sungwuk <psyy94@naver.com>
2017-11-01 18:23:03 +0900

    red

--- index.js
+++ index.js
@@ -249,3 +249,18 @@ exports.decorateTerm = (Term, { React, notify }) => {
     }
   }
 };
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    borderColor: 'red',
+    cursorColor: 'red',
+    css: `
+      ${config.css || ''}
+      .tabs_nav .tabs_list .tab_text {
+        color: red;
+      }
+      .tabs_nav .tabs_title {
+        color: red;
+      }
+    `
+  });
+}
